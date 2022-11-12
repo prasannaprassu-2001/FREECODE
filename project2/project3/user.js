b88047441c6aba4c896dd527c82f6a8aec404abf
prasannaprassu-2001 <amuprassu11@gmail.com>
2022-11-12 12:54:32 +0530

    total

--- project2/project3/user.js
+++ project2/project3/user.js
@@ -1,8 +1,29 @@
-function age(a){
+/*function age(a){
   let present_Days = (365*90) - (a*365);
   let present_Weeks =  (52*90) - (a*52);
   let present_Months = (12*90) - (a*12);
   console.log("you have" +present_Days+ "days," +present_Weeks+ "weeks," +present_Months+ "months left");
 }
 
-age(21)
+age(21)*/
+function lifeInWeeks(age) {
+    
+    /*****Don't change the code above*****/    
+    var total_Days=(90*365);
+    var present_Days=(age*365);
+        remaining_Days=(total_Days - present_Days);
+        
+    var total_Weeks=(90*52);
+    var present_Weeks=(age*52);
+        remaining_Weeks=(total_Weeks - present_Weeks);
+    
+    
+        var total_Months=(90*12);
+    var present_Months=(age*12);
+        remaining_Months=(total_Months - present_Months);
+        
+        
+        console.log("You have " + remaining_Days + " days " + remaining_Weeks +" weeks, and " +remaining_Months +" months left.")
+         
+        
+        
