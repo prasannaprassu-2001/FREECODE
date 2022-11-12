c49f086009f761c8c993987c206815633d3ae3b1
prasannaprassu-2001 <amuprassu11@gmail.com>
2022-11-12 12:44:34 +0530

    age

--- project2/project3/user.js
+++ project2/project3/user.js
@@ -1,8 +1,8 @@
-var i = 1
-function mul(){
-
-console.log("multiply", i);
-i++
-mul()
+function age(a){
+  let present_Days = (365*90) - (a*365);
+  let present_Weeks =  (52*90) - (a*52);
+  let present_Months = (12*90) - (a*12);
+  console.log("you have" +present_Days+ "days," +present_Weeks+ "weeks," +present_Months+ "months left");
 }
-mul();
+
+age(21)
