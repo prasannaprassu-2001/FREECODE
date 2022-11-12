506e161d8b320d0c1362f06428a0bec442646b5a
prasannaprassu-2001 <amuprassu11@gmail.com>
2022-11-12 11:04:02 +0530

    user

--- project2/project3/user.js
+++ project2/project3/user.js
@@ -1,5 +1,9 @@
-function sub(a, b, c, d=100){
+function sub(id, name, sal){
    
-    console.log(a+b+c+d);
+    console.log(id);
+    console.log(name);
+    console.log(sal+5000);
+    
 }
- sub(10, 20, 40, 30);
+sub(101, "Vidya", 25000);
+sub(102, "chana", 25000)
