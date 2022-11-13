29f759e33fa27abb7593e9b063fa4cfab4299ae8
prasannaprassu-2001 <amuprassu11@gmail.com>
2022-11-14 00:10:05 +0530

    st

--- project2/project3/statement.js
+++ project2/project3/statement.js
@@ -1,10 +1,14 @@
-function age(){
+function result(){
     
-    if(document.getElementById("t1").value>=20)
+    if(document.getElementById("t1").value==document.getElementById("t2").value)
     {
-   document.getElementById("r1").innerHTML="eligible to vote"
+   document.getElementById("r1").innerHTML="both are equal"
+    }
+    else if(document.getElementById("t1").value>document.getElementById("t2").value)
+    {
+        document.getElementById("r1").innerHTML="First no. is greater than second no."
     }
     else{
-        document.getElementById("r1").innerHTML="Not eligible to vote"
+        document.getElementById("r1").innerHTML="First no. is smaller than second no." 
     }
 }
