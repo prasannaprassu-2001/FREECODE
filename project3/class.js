e31c3d3a7aa0bf613c50422ad8ab55ae7b82cd20
prasannaprassu-2001 <amuprassu11@gmail.com>
2022-11-15 13:09:10 +0530

    aa

--- project3/class.js
+++ project3/class.js
@@ -1,9 +1,9 @@
 class student{
-student_name ;
-student_rno ;
-student_email;
-student_phoneno;
-student_fees;
+s_name ;
+s_rno ;
+s_email;
+s_phoneno;
+s_fees;
 student_reading(){
 console.log("Reading Maths Books")
 }
@@ -12,16 +12,24 @@ student_writing(){
 }
  
 constructor(name, rno, email, phoneno, fees){
-this.student_name = name;
-this.student_rno = rno;
-this.student_email = email;
-this.student_phoneno = phoneno;
-this.student_fees = fees;
+this.s_name = name;
+this.s_rno = rno;
+this.s_email = email;
+this.s_phoneno = phoneno;
+this.s_fees = fees;
 }
 fees(fee){
-    return this.student_fees = fee + 5000;
+    return this.s_fees = fee + 5000;
     
     } 
+}
+class chstudent extends student{
+    s1_address;
+constructor(name, rno, email, phoneno, fees, address){
+    super(name, rno, email, phoneno, fees)
+    this.s1_address = address;
+    
+}
 }
 let a=new student();
 console.log(a)
@@ -37,3 +45,14 @@ a.student_reading();
 a.student_writing();
 a.fees(20000)
 console.log(a)
+
+let b=new chstudent();
+console.log(b)
+let b1=new chstudent("vidya", 187, "vidyac122@gmail.com", 8088296882, 20000, "Hosur") 
+let b2=new chstudent("chana", 161, "chana123@gmail.com", 7338456901, 20000, "tirupati")
+let b3=new chstudent("ashok", 280, "ashok2342@gmail.com", 6363191963, 20000, "Bangalore")
+let b4=new chstudent("teju", 287, "teju232@gmail.com", 987654321, 20000, "Chitradurga") 
+console.log(b1)
+console.log(b2)
+console.log(b3)
+console.log(b4)
