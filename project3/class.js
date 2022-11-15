95da0250c2991c672f5ff9a1333a5b4b80adbc0f
prasannaprassu-2001 <amuprassu11@gmail.com>
2022-11-15 11:03:50 +0530

    class

---
+++ project3/class.js
@@ -0,0 +1,8 @@
+class student{
+student_name = ["Vidya", "chana", "ashok", "teju"];
+student_rno = [187, 161, 280, 257];
+student_email="vidyac12@gmail.com";
+student_phoneno = 8088289266
+}
+let a=new student();
+console.log(a)
