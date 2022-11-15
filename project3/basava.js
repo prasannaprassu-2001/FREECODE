b80a37d549f5a45dbe1c928a8cac3869bf582bf9
prasannaprassu-2001 <amuprassu11@gmail.com>
2022-11-15 18:23:05 +0530

    f

--- project3/basava.js
+++ project3/basava.js
@@ -1,4 +1,4 @@
-function add(a, b, c){
+/* function add(a="prasanna", b="vidya", c="chana"){
 if(a==10){
 return 0;
 }else if(b==10){
@@ -13,4 +13,21 @@ return a;
 console.log(add(10, 20, 30))
 console.log(add(20,10,30))
 console.log(add(20,30,10))
-console.log(add(20,30,40))
+console.log(add(20,30,40)) */
+
+/* function add(){
+for(i=0;i<=50;i++){
+    console.log("Hello,World")
+}
+}
+add("Hello, World") */
+
+function add(a, b){
+if(a!=b){
+   return true;
+}else if(a==b){
+   return false;
+}
+}
+console.log(add(10, 10))
+console.log(add(10, 20))
